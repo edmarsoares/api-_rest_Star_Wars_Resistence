1d23694529eb2c8f9a6f4ccd10a0e969ee7c0f86
Edmar Soares <edmarlima>
2019-11-29 16:35:34 -0300

    adicionando pacotes iniciais

---
+++ src/main/java/com/edmar/apiStarWarsResistence/ApiStarWarsResistenceApplication.java
@@ -0,0 +1,13 @@
+package com.edmar.apiStarWarsResistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStarWarsResistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStarWarsResistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edmar/apiStarWarsResistence/ApiStarWarsResistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmar.apiStarWarsResistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStarWarsResistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
